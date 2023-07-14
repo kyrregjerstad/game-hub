97eea41d62d5fa561f3148442c7165c13a5c9ea4
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-14 15:41:53 +0200

    refac remove dup interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,7 @@ import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 
 import { HStack, Icon, Text } from "@chakra-ui/react";
-import type { Platform } from "../hooks/useGames";
+import type { Platform } from "../hooks/usePlatforms";
 import { IconType } from "react-icons";
 
 interface Props {
