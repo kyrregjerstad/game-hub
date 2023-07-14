389e75f62d4214eb04a53acb2c9d82cace8baacb
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-14 16:49:27 +0200

    refac simplify time calcs

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -14,10 +15,8 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
     initialData: platforms,
   });
-
-// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 export default usePlatforms;
