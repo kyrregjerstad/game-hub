5d0841e00434fdf921b0acb3b0f2523c8dc80c2e
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-16 12:37:56 +0200

    fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,6 +13,7 @@ import { PlatformIconList } from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import { Emoji } from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -27,7 +28,7 @@ export const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize={"2xl"}>
-          {game.name}
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,15 @@ interface Props {
 
 export const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden" width="100%">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      width="100%"
+      _hover={{
+        transform: "scale(1.01)",
+        transition: "transform 0.2s ease-in-out",
+      }}
+    >
       {children}
     </Box>
   );
