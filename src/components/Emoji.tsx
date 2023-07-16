9fa33bd50b5671fa6c005a57bfcc1279c7bcc569
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-16 17:59:14 +0200

    add fire rating

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,20 +1,21 @@
-import bullsEye from "../assets/Emojis/bulls-eye.webp";
-import thumbsUp from "../assets/Emojis/thumbs-up.webp";
-import meh from "../assets/Emojis/meh.webp";
-import { Image, ImageProps } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
+import fire from "../assets/Emojis/fire.webp";
+import fireGrey from "../assets/Emojis/fireGrey.webp";
 
 interface Props {
   rating: number;
 }
 
 export const Emoji = ({ rating }: Props) => {
-  if (rating < 3) return null;
-
-  const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: "meh", boxSize: "25px" },
-    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
-    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
-  };
-
-  return <Image {...emojiMap[rating]} marginTop={2} borderRadius={8}></Image>;
+  const totalImages = 5;
+  return (
+    <Flex>
+      {[...Array(totalImages)].map((_, i) => {
+        const imgSrc = i < rating ? fire : fireGrey;
+        return (
+          <Image key={i} src={imgSrc} boxSize={"25px"} marginTop={2} borderRadius={8} />
+        );
+      })}
+    </Flex>
+  );
 };
